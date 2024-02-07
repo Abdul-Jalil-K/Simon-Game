818b248c360e74ff5944242776915e79a7b61465
Abdul-Jalil-K <133749289+Abdul-Jalil-K@users.noreply.github.com>
2024-02-07 14:16:37 +0300

    Add files via upload

---
+++ game.js
@@ -0,0 +1,115 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+var userClickedPattern = [];
+
+var started = false;
+var level = 0;
+
+$(document).keydown(function() {
+    if (!started) {
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started = true;
+    }
+});
+
+
+
+$(".btn").click(function() {
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+
+    checkAnswer(userClickedPattern.length-1);
+
+
+})
+
+
+function checkAnswer(currentLevel) {
+
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+        
+        console.log("success");
+
+        if (userClickedPattern.length === gamePattern.length) {
+            setTimeout(function() {
+                nextSequence();
+            }, 1000);
+        }
+    } else {
+
+        $("#level-title").text("Game Over, Press Any Key To Restart.");
+
+        $("body").addClass("game-over");
+
+        setTimeout(function() {
+            $("body").removeClass("game-over");
+        }, 200);
+
+
+        var wrong = new Audio("sounds/wrong.mp3");
+        wrong.play();
+
+        startOver();
+
+        console.log("wrong");
+
+    }
+
+}
+
+
+
+
+function nextSequence() {
+
+    userClickedPattern = [];
+
+    level++;
+    $("#level-title").text("Level " + level);
+
+    var randomNumberGenerator = Math.floor(Math.random() * 4);
+    var randomChosenColour = buttonColours[randomNumberGenerator];
+    gamePattern.push(randomChosenColour);
+
+
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColour);
+    
+}
+
+function startOver() {
+
+        level = 0;
+        started = false;
+        gamePattern = [];
+
+}
+
+function playSound(name) {
+
+    this.name = userClickedPattern;
+
+    var audio = new Audio("sounds/" + name + ".mp3")
+    audio.play();
+
+
+}
+
+function animatePress(currentColor) {
+    $("#" + currentColor).addClass("pressed");
+
+    setTimeout(function() {
+        $("#" + currentColor).removeClass("pressed");
+    }, 100);
+
+
+}
+
+
+
+
